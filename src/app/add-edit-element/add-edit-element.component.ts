c2fc0a3f07f41d2957cbc9b1936c70ed71676fad
Jul Jan <julia.janus114@gmail.com>
2023-05-10 13:34:31 +0200

    vehicle service implemented

--- src/app/add-edit-element/add-edit-element.component.ts
+++ src/app/add-edit-element/add-edit-element.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { DialogRef } from '@angular/cdk/dialog';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { VehicleService } from '../services/vehicle.service';
 
 @Component({
   selector: 'app-add-edit-element',
   templateUrl: './add-edit-element.component.html',
   styleUrls: ['./add-edit-element.component.scss']
 })
-export class AddEditElementComponent {
+export class AddEditElementComponent implements OnInit {
+  truckForm: FormGroup;
   trailerTypes: string[] = [
     "flatbed",
     "lowboy",
@@ -15,4 +19,35 @@ export class AddEditElementComponent {
     "tanker",
     "tipper"
   ]
+
+  constructor(private _form: FormBuilder, private _vehicleService: VehicleService, private _dialogRef: DialogRef<AddEditElementComponent>) {
+    this.truckForm = this._form.group({
+      truckPlate:'',
+      semitrailerPlate: '',
+      semitrailerType: '',
+      truckService: '',
+      trailerService: '',
+      truckInsurance: '',
+      trailerInsurance: '',
+      driversName: '',
+      driversSurname: ''
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  onFormSubmit() {
+    if (this.truckForm.valid) {
+      this._vehicleService.addVehicle(this.truckForm.value).subscribe({
+        next: (value: any) => {
+          alert('vehicle added succesfuly');
+          this._dialogRef.close();
+        },
+        error: (err: any) => {
+          console.log(err);
+        }
+      })
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AddEditElementComponent } from './add-edit-element/add-edit-element.component';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,14 +9,13 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
-
-
-
+import { AppComponent } from './app.component';
+import { AddEditElementComponent } from './add-edit-element/add-edit-element.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,7 +33,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
