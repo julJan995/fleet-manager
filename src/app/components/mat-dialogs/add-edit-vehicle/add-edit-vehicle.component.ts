97480a46e5b8a02cc8aae62895f9b2f7d6fad954
Jul Jan <julia.janus114@gmail.com>
2023-06-02 12:58:36 +0200

    vehicles part with the table and form completed

--- src/app/components/mat-dialogs/add-edit-vehicle/add-edit-vehicle.component.ts
+++ src/app/components/mat-dialogs/add-edit-vehicle/add-edit-vehicle.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Vehicle } from 'src/app/models';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { VehicleService } from 'src/app/services/vehicle.service';
 
 @Component({
   selector: 'app-add-edit-vehicle',
   templateUrl: './add-edit-vehicle.component.html',
   styleUrls: ['./add-edit-vehicle.component.scss']
 })
-export class AddEditVehicleComponent {
+export class AddEditVehicleComponent implements OnInit {
+  vehicleForm: FormGroup;
 
+  constructor(
+    private _form: FormBuilder,
+    private _vehicleService: VehicleService,
+    private _snackBarService: SnackbarService,
+    private _dialogRef: MatDialogRef<AddEditVehicleComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.vehicleForm = this._form.group({
+      truckPlate: '',
+      truckService: '',
+      truckInsurance: ''
+    })
+  }
+  ngOnInit(): void {
+      this.vehicleForm.patchValue(this.data);
+  }
+
+  get newVehiclePayload(): Vehicle {
+    return {
+      ...this.vehicleForm.value
+    }
+  }
+
+  onVehicleFormSubmit() {
+    if (this.vehicleForm.valid) {
+      if (this.data) {
+        this._vehicleService
+          .updateVehiclesList(this.data.id, this.vehicleForm.value)
+          .subscribe({
+            next: (value: any) => {
+              this._snackBarService.openSnackBar('Vehicle added successfully', 'done')
+              this._dialogRef.close(true)
+            },
+            error: (err: any) => {
+              console.log(err);
+            }
+          })
+      } else {
+        this._vehicleService
+          .addVehicle(this.newVehiclePayload)
+          .subscribe({
+            next: (value: any) => {
+              this._snackBarService.openSnackBar('Vehicle added successfully', 'done')
+              this._dialogRef.close(true);
+            },
+            error: (err: any) => {
+              console.log(err);
+            }
+          })
+      }
+    }
+  }
 }
--- src/app/components/vehicles/vehicles.component.ts
+++ src/app/components/vehicles/vehicles.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { SnackbarService } from 'src/app/services/snackbar.service';
 import { VehicleService } from 'src/app/services/vehicle.service';
+import { AddEditVehicleComponent } from '../mat-dialogs/add-edit-vehicle/add-edit-vehicle.component';
 
 @Component({
   selector: 'app-vehicles',
@@ -16,6 +20,10 @@ export class VehiclesComponent implements OnInit {
     'action',
   ];
 
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(
     private _dialog: MatDialog,
     private _vehicleService: VehicleService,
@@ -24,5 +32,60 @@ export class VehiclesComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  openAddVehicleForm() {
+    const dialogRef = this._dialog.open(AddEditVehicleComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (value) => {
+        if (value) {
+          this.getVehiclesList();
+        }
+      }
+    })
+  }
+
+  getVehiclesList() {
+    this._vehicleService.getVehiclesList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => {
+        console.log('error occured', err);
+      }
+    })
+  }
+
+  deleteVehicle(id: number) {
+    this._vehicleService.deleteVehicle(id).subscribe({
+      next: (response) => {
+        this._snackBarService.openSnackBar('Vehicle deleted', 'done')
+        this.getVehiclesList();
+      },
+      error: console.log,
+    })
+  }
+
+  openEditForm(data: any) {
+    const dialogRef = this._dialog.open(AddEditVehicleComponent, {
+      data,
+    })
+    dialogRef.afterClosed().subscribe({
+      next: (value) => {
+        if (value) {
+          this.getVehiclesList();
+        }
+      }
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
