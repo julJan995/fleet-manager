15df4c87a61145d8ec80cd41a2fca3c7a425304a
Jul Jan <julia.janus114@gmail.com>
2023-06-02 16:59:42 +0200

    semitrailers template form

--- src/app/components/semitrailers/semitrailers.component.ts
+++ src/app/components/semitrailers/semitrailers.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { SemitrailerService } from 'src/app/services/semitrailer.service';
 import { SnackbarService } from 'src/app/services/snackbar.service';
 import { AddEditSemitrailerComponent } from '../mat-dialogs/add-edit-semitrailer/add-edit-semitrailer.component';
 
@@ -46,7 +47,46 @@ export class SemitrailersComponent implements OnInit {
   }
 
   getSemitrailersList() {
-
+    this._semitrailerService.getSemitrailersList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => {
+        console.log('error occured', err);
+      }
+    })
   }
 
+  deleteSemitrailer(id: number) {
+    this._semitrailerService.deleteSemitrailer(id).subscribe({
+      next: (response) => {
+        this._snackBarService.openSnackBar('Semitrailer deleted', 'done')
+        this.getSemitrailersList();
+      },
+      error: console.log,
+    })
+  }
+
+  openEditForm(data: any) {
+    const dialogRef = this._dialog.open(AddEditSemitrailerComponent, {
+      data,
+    })
+    dialogRef.afterClosed().subscribe({
+      next: (value) => {
+        if (value) {
+          this.getSemitrailersList();
+        }
+      }
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
