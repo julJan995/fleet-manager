00c399c7255c7ba231502596cede54145bbbf57b
juliajanus <julia.janus.dev@gmail.com>
2025-09-23 21:17:22 +0200

    ref: moving car banner to another component to make it reusable

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -7,6 +7,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { CarBannerComponent } from '../../shared/components/car-banner/car-banner.component';
 
 @Component({
   selector: 'app-register',
@@ -18,7 +19,8 @@ import { MatInputModule } from '@angular/material/input';
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    CarBannerComponent
   ],
 })
 export class RegisterComponent {
