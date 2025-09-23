91b61c9eb06b914b42e598afa33f7e328535b2bc
juliajanus <julia.janus.dev@gmail.com>
2025-09-23 15:57:05 +0200

    feat: register implementation

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ export class RegisterComponent {
   fb = inject(FormBuilder);
   http = inject(HttpClient);
   router = inject(Router);
+  authService = inject(AuthService)
 
   form = this.fb.nonNullable.group({
     username: ['', Validators.required],
@@ -22,6 +24,16 @@ export class RegisterComponent {
   errorMessage: string | null = null;
 
   onSubmit(): void {
-    console.log('register');
+    const rawForm = this.form.getRawValue();
+
+    this.authService
+      .register(rawForm.email, rawForm.username, rawForm.password)
+      .subscribe({
+        next: () => { this.router.navigateByUrl('/') },
+        error: (err) =>  {
+          this.errorMessage = err.code;
+          console.error('Navigation error:', err)
+        }
+      });
   }
 }
