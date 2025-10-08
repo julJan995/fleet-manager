6bbd7c092597deb918bbd7e51b3dfb15bcb175f8
juliajanus <julia.janus.dev@gmail.com>
2025-10-08 13:43:50 +0200

    feat: reset form confirmation

--- src/app/features/vehicle-form-dialog/vehicle-form-dialog.component.ts
+++ src/app/features/vehicle-form-dialog/vehicle-form-dialog.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatButtonModule } from '@angular/material/button';
+import {Component, inject, TemplateRef, ViewChild} from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepper, MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialog, MatDialogActions, MatDialogClose} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-vehicle-form-dialog',
@@ -12,13 +13,20 @@ import { MatButtonModule } from '@angular/material/button';
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDialogActions,
+    MatDialogClose
   ],
   templateUrl: './vehicle-form-dialog.component.html',
   styleUrl: './vehicle-form-dialog.component.scss'
 })
 export class VehicleFormDialogComponent {
   private _formBuilder = inject(FormBuilder);
+  private _dialog = inject(MatDialog);
+
+  @ViewChild('stepper') stepper!: MatStepper;
+  @ViewChild('confirmDialog') confirmDialog!: TemplateRef<any>;
+
   linear = false;
 
   vehicleDetailsForm = this._formBuilder.group({
@@ -52,4 +60,17 @@ export class VehicleFormDialogComponent {
     currentValue: [''],
     imgUrl: ['']
   });
+
+  openResetConfirmDialog() {
+    const dialogRef = this._dialog.open(this.confirmDialog, {
+      width: '400px',
+      disableClose: true
+    });
+
+    dialogRef.afterClosed().subscribe((confirmed) => {
+      if (confirmed) {
+        this.stepper.reset();
+      }
+    });
+  }
 }
